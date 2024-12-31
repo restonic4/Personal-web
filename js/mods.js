a0982046a67ac6877c8b8fe8ef534f3c44d4e87f
restonic4 <marcosruizlerma@gmail.com>
2024-12-31 15:26:55 +0100

    extra info

--- js/mods.js
+++ js/mods.js
@@ -10,5 +10,4 @@ function calculatePercentage(modPosition, lastPosition) {
 }
 
 const percentage = calculatePercentage(2836, 33720);
-const percentage2 = calculatePercentage(42412, 32193717);
-console.log(`Mod stats: ${percentage.toFixed(2)}% and ${percentage2.toFixed(2)}%`);
+console.log(`Mod stats: ${percentage.toFixed(2)}%`);
