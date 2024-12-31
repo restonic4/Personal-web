338c8cb3fd7dcc31648e035a7b4d6a4c41b1a2e1
restonic4 <marcosruizlerma@gmail.com>
2024-12-31 15:19:37 +0100

    Test

---
+++ js/mods.js
@@ -0,0 +1,13 @@
+function calculatePercentage(modPosition, lastPosition) {
+    if (modPosition < 1 || lastPosition < 1) {
+        console.log("Must be positive numbers.");
+        return;
+    }
+
+    const percentage = ((lastPosition - modPosition) / (lastPosition - 1)) * 100;
+
+    return Math.min(Math.max(percentage, 0), 100);
+}
+
+const percentage = calcularPorcentaje(2836, 33720);
+console.log(`Mod stats: ${percentage.toFixed(2)}%`);
